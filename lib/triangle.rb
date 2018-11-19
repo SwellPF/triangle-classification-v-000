1570a9b17ab9bf837be91c2f9ec3730eeae8a250
Paul Fell-Cinoman <swellpf@gmail.com>
2018-11-19 17:48:37 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,8 +9,9 @@ class Triangle
   end
   
   def kind
-  # binding.pry
-    if side1 <= 0 || side2 <= 0 || side3 <= 0
+   binding.pry
+    sides = Array.new(side1, side2, side3)
+    if (side1 <= 0 || side2 <= 0 || side3 <= 0) || 
           raise TriangleError
     elsif side1 == side2 && side2 == side3
       :equilateral
