0b6565cddc53022ca97b93e2bc68a20771eca12d
Paul Fell-Cinoman <swellpf@gmail.com>
2018-11-19 17:30:14 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,9 +12,6 @@ class Triangle
   # binding.pry
     if side1 <= 0 || side2 <= 0 || side3 <= 0
           raise TriangleError
-          rescue TriangleError => error
-            puts error.message
-        end
     elsif side1 == side2 && side2 == side3
       :equilateral
     elsif side1 == side2 || side1 == side3 || side2 == side3
